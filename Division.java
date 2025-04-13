757fc64f5dca6ea90396386c04c290f2579c1c99
codeneat9 <yashwithbehara18@gmail.com>
2025-04-13 20:33:34 +0530

    Taking inputs from user and handling exceptions- Division

--- Division.java
+++ Division.java
@@ -1,3 +1,17 @@
 import java.util.Scanner;
 
 public class Division {
+    public static void perform(Scanner scanner) {
+        try {
+            System.out.print("Enter two numbers to divide: ");
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            if (b == 0) throw new ArithmeticException("Cannot divide by zero!");
+            System.out.println("Result: " + (a / b));
+        } catch (ArithmeticException ae) {
+            throw ae;
+        } catch (Exception e) {
+            System.out.println("Error in Division: " + e.getMessage());
+        }
+    }
+}
