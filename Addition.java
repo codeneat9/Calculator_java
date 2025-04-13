4df061adf73997619f676145ef0569721095a248
codeneat9 <yashwithbehara18@gmail.com>
2025-04-13 20:31:30 +0530

    Taking user's input and handling exceptions

--- Addition.java
+++ Addition.java
@@ -1,3 +1,14 @@
 import java.util.Scanner;
 
 public class Addition {
+    public static void perform(Scanner scanner) {
+        try {
+            System.out.print("Enter two numbers to add: ");
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            System.out.println("Result: " + (a + b));
+        } catch (Exception e) {
+            System.out.println("Error in Addition: " + e.getMessage());
+        }
+    }
+}
