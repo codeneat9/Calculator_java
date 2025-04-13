7c7d9e9bdcff54cb8bdd5b6c3dae97518fc28dca
codeneat9 <yashwithbehara18@gmail.com>
2025-04-13 20:36:53 +0530

    Taking user's input and handling exceptions - Square Root operation

--- SquareRoot.java
+++ SquareRoot.java
@@ -1,3 +1,16 @@
 import java.util.Scanner;
 
 public class SquareRoot {
+    public static void perform(Scanner scanner) {
+        try {
+            System.out.print("Enter a number to find the square root: ");
+            double a = scanner.nextDouble();
+            if (a < 0) throw new ArithmeticException("Cannot calculate square root of a negative number!");
+            System.out.println("Result: " + Math.sqrt(a));
+        } catch (ArithmeticException ae) {
+            throw ae;
+        } catch (Exception e) {
+            System.out.println("Error in SquareRoot: " + e.getMessage());
+        }
+    }
+}
