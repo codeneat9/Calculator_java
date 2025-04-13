db522e791abfd5d657f912d8cf1b690c7e044d72
codeneat9 <yashwithbehara18@gmail.com>
2025-04-13 20:35:43 +0530

    Taking inputs from user and handling exceptions-Square

--- Square.java
+++ Square.java
@@ -1,3 +1,13 @@
 import java.util.Scanner;
 
 public class Square {
+    public static void perform(Scanner scanner) {
+        try {
+            System.out.print("Enter a number to square: ");
+            double a = scanner.nextDouble();
+            System.out.println("Result: " + (a * a));
+        } catch (Exception e) {
+            System.out.println("Error in Square: " + e.getMessage());
+        }
+    }
+}
