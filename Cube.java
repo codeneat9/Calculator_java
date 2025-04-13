f98a5b6f74aa5203b5719b205dfe34614e544900
codeneat9 <yashwithbehara18@gmail.com>
2025-04-13 20:32:31 +0530

    Taking user inputs and handling exceptions- cube

--- Cube.java
+++ Cube.java
@@ -1,3 +1,14 @@
 import java.util.Scanner;
 
 public class Cube {
+    public static void perform(Scanner scanner) {
+        try {
+            System.out.print("Enter a number to cube: ");
+            double a = scanner.nextDouble();
+            System.out.println("Result: " + (a * a * a));
+        } catch (Exception e) {
+            System.out.println("Error in Cube: " + e.getMessage());
+        }
+    }
+}
+
